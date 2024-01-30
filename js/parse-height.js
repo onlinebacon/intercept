f079e7dad2c3d9c8e531d7523a0965364d299226
bacon <bacon.antife@gmail.com>
2024-01-30 20:32:08 -0300

    Changing indentation to tabs

--- js/parse-height.js
+++ js/parse-height.js
@@ -1,20 +1,20 @@
 const validRegex = /^\d+(\.\d+)?\s*(m|ft)$/;
 
 export const parseHeight = (height) => {
-    const trim = height.trim().toLowerCase();
-    if (!validRegex.test(trim)) {
-        return NaN;
-    }
-    const [ strNum, unit ] = trim.split(/\s+/);
-    const num = Number(strNum);
-    if (isNaN(num)) {
-        return NaN;
-    }
-    if (unit === 'm') {
-        return num;
-    }
-    if (unit === 'ft') {
-        return num * 0.3048;
-    }
-    return NaN;
+	const trim = height.trim().toLowerCase();
+	if (!validRegex.test(trim)) {
+		return NaN;
+	}
+	const [ strNum, unit ] = trim.split(/\s+/);
+	const num = Number(strNum);
+	if (isNaN(num)) {
+		return NaN;
+	}
+	if (unit === 'm') {
+		return num;
+	}
+	if (unit === 'ft') {
+		return num * 0.3048;
+	}
+	return NaN;
 };
