29d741f4184ff23d44a6350847ad6b5907eca07b
Bacon <bacon.antife@gmail.com>
2024-05-02 03:32:38 -0300

    Added mean/SD

--- js/script/analysis.js
+++ js/script/analysis.js
@@ -6,15 +6,23 @@ import { haversine } from '../lib/js/sphere-math.js';
 import { EARTH_AV_RAD_METERS } from '../constants.js';
 import { round } from '../utils.js';
 
+const sum = (a, b) => a + b;
+
 const showErrors = (ctx, coord) => {
     const { lops, labels } = ctx;
     const formatter = ctx.angleFormatter.forcingSign(true);
+    const all = [];
     for (let i=0; i<lops.length; ++i) {
         const lop = lops[i];
         const label = labels[i] ?? (lop instanceof AzLoP ? '*Az' : '*CoP');
         const err = toDeg(lop.error(coord));
+        all[i] = err;
         writeln(i + 1, '. ', formatter.format(err), ' // ' + label);
     }
+    const n = all.length;
+    const mean = all.reduce(sum, 0) / n;
+    const sd = (all.map((val) => (val - mean) ** 2).reduce(sum, 0) / n) ** 0.5;
+    writeln('Mean/SD: ', formatter.format(mean), ', ', formatter.format(sd));
 };
 
 export const logLoPErrors = (ctx = new ExecutionContext()) => {
